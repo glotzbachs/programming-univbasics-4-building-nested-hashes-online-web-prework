00e63bd0ced5181754fd688abca1becdbb48b313
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-09-21 16:22:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,10 +37,10 @@ def monopoly_with_third_tier
       },
       :names=>
       {
-        :reading_railroad+=>{},
-        :pennsylvania_railroad+=>{},
-        :b_and_o_railroad+=>{},
-        :shortline_railroad+=>{}
+        :reading_railroad=>{},
+        :pennsylvania_railroad=>{},
+        :b_and_o_railroad=>{},
+        :shortline_railroad=>{}
       }
     }
   }
@@ -62,16 +62,16 @@ def monopoly_with_fourth_tier
         :four_pieces_owned=>200
       },
       :names=>{
-        :reading_railroad+=>{
+        :reading_railroad=>{
           "mortage_value"=>"$100"
         },
-        :pennsylvania_railroad+=>{
+        :pennsylvania_railroad=>{
           "mortage_value"=>"$200"
         },
-        :b_and_o_railroad+=>{
+        :b_and_o_railroad=>{
           "mortage_value"=>"$400"
         },
-        :shortline_railroad+=>{
+        :shortline_railroad=>{
           "mortage_value"=>"$800"
         }
       }
